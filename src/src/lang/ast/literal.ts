58e9c7b19a766c4ecd40d7f25fd391beecbad1f3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-09-30 12:26:34 +0530

    added function and interger literal

--- src/src/lang/ast/literal.ts
+++ src/src/lang/ast/literal.ts
@@ -1,4 +1,5 @@
-import { Expression } from "./ast";
+import { Expression, Identifier } from "./ast";
+import { BlockStatement } from "./statement.ts";
 import { Token } from "../token/token";
 
 /**
@@ -99,5 +100,81 @@ export class HashLiteral implements Expression {
       })
       .join(", ");
     return `{${pairs}}`;
+  }
+}
+
+/**
+ * Represents an integer literal in the AST.
+ */
+export class IntegerLiteral implements Expression {
+  token: Token;
+  value: number;
+
+  /**
+   * Creates a new IntegerLiteral instance.
+   * @param token The token associated with this literal.
+   * @param value The numeric value of the integer.
+   */
+  constructor(token: Token, value: number) {
+    this.token = token;
+    this.value = value;
+  }
+
+  expressionNode() {}
+
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
+
+  /**
+   * Returns a string representation of the IntegerLiteral.
+   * @returns A string representation of the integer value.
+   */
+  toString(): string {
+    return this.token.literal;
+  }
+}
+
+/**
+ * Represents a function literal in the AST.
+ */
+export class FunctionLiteral implements Expression {
+  token: Token;
+  parameters: Identifier[];
+  body: BlockStatement;
+
+  /**
+   * Creates a new FunctionLiteral instance.
+   * @param token The token associated with this literal.
+   * @param parameters An array of Identifier objects representing the function parameters.
+   * @param body The BlockStatement representing the function body.
+   */
+  constructor(token: Token, parameters: Identifier[], body: BlockStatement) {
+    this.token = token;
+    this.parameters = parameters;
+    this.body = body;
+  }
+
+  expressionNode() {}
+
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
+
+  /**
+   * Returns a string representation of the FunctionLiteral.
+   * @returns A string representation of the function, including parameters and body.
+   */
+  toString(): string {
+    const params = this.parameters.map((p) => p.toString()).join(", ");
+    return `${this.tokenLiteral()}(${params}) ${this.body.toString()}`;
   }
 }
