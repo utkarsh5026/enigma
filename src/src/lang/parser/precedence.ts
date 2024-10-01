da6e0094ec7d1ec2180bca9175de26714567e787
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-10-01 12:29:29 +0530

    changed assign precedence

--- src/src/lang/parser/precedence.ts
+++ src/src/lang/parser/precedence.ts
@@ -1,29 +1,30 @@
-import {TokenType} from "../token/token";
+import { TokenType } from "../token/token";
 
 export enum Precedence {
-    LOWEST,
-    EQUALS, // ==
-    LESS_GREATER, // > or <
-    SUM, // +
-    PRODUCT, // *
-    PREFIX, // -X or !X
-    CALL, // myFunction(X)
-    INDEX, // array[index]
+  LOWEST,
+  EQUALS, // ==
+  LESS_GREATER, // > or <
+  SUM, // +
+  PRODUCT, // *
+  PREFIX, // -X or !X
+  CALL, // myFunction(X)
+  INDEX, // array[index]
 }
 
 const precedences: { [key in TokenType]?: Precedence } = {
-    [TokenType.EQ]: Precedence.EQUALS,
-    [TokenType.NOT_EQ]: Precedence.EQUALS,
-    [TokenType.LESS_THAN]: Precedence.LESS_GREATER,
-    [TokenType.GREATER_THAN]: Precedence.LESS_GREATER,
-    [TokenType.PLUS]: Precedence.SUM,
-    [TokenType.MINUS]: Precedence.SUM,
-    [TokenType.SLASH]: Precedence.PRODUCT,
-    [TokenType.ASTERISK]: Precedence.PRODUCT,
-    [TokenType.LPAREN]: Precedence.CALL,
-    [TokenType.LBRACKET]: Precedence.INDEX,
+  [TokenType.EQ]: Precedence.EQUALS,
+  [TokenType.ASSIGN]: Precedence.EQUALS,
+  [TokenType.NOT_EQ]: Precedence.EQUALS,
+  [TokenType.LESS_THAN]: Precedence.LESS_GREATER,
+  [TokenType.GREATER_THAN]: Precedence.LESS_GREATER,
+  [TokenType.PLUS]: Precedence.SUM,
+  [TokenType.MINUS]: Precedence.SUM,
+  [TokenType.SLASH]: Precedence.PRODUCT,
+  [TokenType.ASTERISK]: Precedence.PRODUCT,
+  [TokenType.LPAREN]: Precedence.CALL,
+  [TokenType.LBRACKET]: Precedence.INDEX,
 };
 
 export function getPrecedence(tokenType: TokenType): Precedence {
-    return precedences[tokenType] || Precedence.LOWEST;
+  return precedences[tokenType] ?? Precedence.LOWEST;
 }
