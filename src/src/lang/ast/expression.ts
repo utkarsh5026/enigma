b27a0a22cf6073333159564c526ae6b7782589a0
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-10-01 12:56:29 +0530

    added assignment expression

--- src/src/lang/ast/expression.ts
+++ src/src/lang/ast/expression.ts
@@ -1,184 +1,180 @@
-import {Expression} from "./ast.ts";
-import {Token} from "../token/token.ts";
-import {BlockStatement} from "./statement.ts";
+import { Expression, Identifier } from "./ast.ts";
+import { Token } from "../token/token.ts";
+import { BlockStatement } from "./statement.ts";
 
 /**
  * Represents a prefix expression in the AST.
  */
 export class PrefixExpression implements Expression {
-    token: Token;
-    operator: string;
-    right: Expression;
+  token: Token;
+  operator: string;
+  right: Expression;
 
-    /**
-     * Creates a new PrefixExpression instance.
-     * @param token The token associated with this expression.
-     * @param operator The prefix operator.
-     * @param right The expression to the right of the operator.
-     */
-    constructor(token: Token, operator: string, right: Expression) {
-        this.token = token;
-        this.operator = operator;
-        this.right = right;
-    }
+  /**
+   * Creates a new PrefixExpression instance.
+   * @param token The token associated with this expression.
+   * @param operator The prefix operator.
+   * @param right The expression to the right of the operator.
+   */
+  constructor(token: Token, operator: string, right: Expression) {
+    this.token = token;
+    this.operator = operator;
+    this.right = right;
+  }
 
-    expressionNode() {
-    }
+  expressionNode() {}
 
-    /**
-     * Returns the literal value of the token.
-     * @returns The literal value of the token.
-     */
-    tokenLiteral(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
 
-    /**
-     * Returns a string representation of the PrefixExpression.
-     * @returns A string representation of the expression.
-     */
-    toString(): string {
-        return `(${this.operator}${this.right.toString()})`;
-    }
+  /**
+   * Returns a string representation of the PrefixExpression.
+   * @returns A string representation of the expression.
+   */
+  toString(): string {
+    return `(${this.operator}${this.right.toString()})`;
+  }
 }
 
 /**
  * Represents an infix expression in the AST.
  */
 export class InfixExpression implements Expression {
-    token: Token;
-    left: Expression;
-    operator: string;
-    right: Expression;
+  token: Token;
+  left: Expression;
+  operator: string;
+  right: Expression;
 
-    /**
-     * Creates a new InfixExpression instance.
-     * @param token The token associated with this expression.
-     * @param left The expression to the left of the operator.
-     * @param operator The infix operator.
-     * @param right The expression to the right of the operator.
-     */
-    constructor(
-        token: Token,
-        left: Expression,
-        operator: string,
-        right: Expression
-    ) {
-        this.token = token;
-        this.left = left;
-        this.operator = operator;
-        this.right = right;
-    }
+  /**
+   * Creates a new InfixExpression instance.
+   * @param token The token associated with this expression.
+   * @param left The expression to the left of the operator.
+   * @param operator The infix operator.
+   * @param right The expression to the right of the operator.
+   */
+  constructor(
+    token: Token,
+    left: Expression,
+    operator: string,
+    right: Expression
+  ) {
+    this.token = token;
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
 
-    expressionNode() {
-    }
+  expressionNode() {}
 
-    /**
-     * Returns the literal value of the token.
-     * @returns The literal value of the token.
-     */
-    tokenLiteral(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
 
-    /**
-     * Returns a string representation of the InfixExpression.
-     * @returns A string representation of the expression.
-     */
-    toString(): string {
-        const left = this.left.toString();
-        const right = this.right.toString();
-        return `(${left} ${this.operator} ${right})`;
-    }
+  /**
+   * Returns a string representation of the InfixExpression.
+   * @returns A string representation of the expression.
+   */
+  toString(): string {
+    const left = this.left.toString();
+    const right = this.right.toString();
+    return `(${left} ${this.operator} ${right})`;
+  }
 }
 
 /**
  * Represents a boolean expression in the AST.
  */
 export class BooleanExpression implements Expression {
-    token: Token;
-    value: boolean;
+  token: Token;
+  value: boolean;
 
-    /**
-     * Creates a new BooleanExpression instance.
-     * @param token The token associated with this expression.
-     * @param value The boolean value of the expression.
-     */
-    constructor(token: Token, value: boolean) {
-        this.token = token;
-        this.value = value;
-    }
+  /**
+   * Creates a new BooleanExpression instance.
+   * @param token The token associated with this expression.
+   * @param value The boolean value of the expression.
+   */
+  constructor(token: Token, value: boolean) {
+    this.token = token;
+    this.value = value;
+  }
 
-    expressionNode() {
-    }
+  expressionNode() {}
 
-    /**
-     * Returns the literal value of the token.
-     * @returns The literal value of the token.
-     */
-    tokenLiteral(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
 
-    /**
-     * Returns a string representation of the BooleanExpression.
-     * @returns A string representation of the expression.
-     */
-    toString(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns a string representation of the BooleanExpression.
+   * @returns A string representation of the expression.
+   */
+  toString(): string {
+    return this.token.literal;
+  }
 }
 
 /**
  * Represents an if-else expression in the AST.
  */
 export class IfExpression implements Expression {
-    token: Token;
-    condition: Expression;
-    consequence: BlockStatement;
-    alternative: BlockStatement | null;
+  token: Token;
+  condition: Expression;
+  consequence: BlockStatement;
+  alternative: BlockStatement | null;
 
-    /**
-     * Creates a new IfExpression instance.
-     * @param token The token associated with this expression.
-     * @param condition The condition of the if statement.
-     * @param consequence The block to execute if the condition is true.
-     * @param alternative The optional block to execute if the condition is false.
-     */
-    constructor(
-        token: Token,
-        condition: Expression,
-        consequence: BlockStatement,
-        alternative: BlockStatement | null
-    ) {
-        this.token = token;
-        this.condition = condition;
-        this.consequence = consequence;
-        this.alternative = alternative;
-    }
+  /**
+   * Creates a new IfExpression instance.
+   * @param token The token associated with this expression.
+   * @param condition The condition of the if statement.
+   * @param consequence The block to execute if the condition is true.
+   * @param alternative The optional block to execute if the condition is false.
+   */
+  constructor(
+    token: Token,
+    condition: Expression,
+    consequence: BlockStatement,
+    alternative: BlockStatement | null
+  ) {
+    this.token = token;
+    this.condition = condition;
+    this.consequence = consequence;
+    this.alternative = alternative;
+  }
 
-    expressionNode() {
-    }
+  expressionNode() {}
 
-    /**
-     * Returns the literal value of the token.
-     * @returns The literal value of the token.
-     */
-    tokenLiteral(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
 
-    /**
-     * Returns a string representation of the IfExpression.
-     * @returns A string representation of the expression.
-     */
-    toString(): string {
-        let out = `if ${this.condition.toString()} ${this.consequence.toString()}`;
-        if (this.alternative !== null) {
-            out += `else ${this.alternative.toString()}`;
-        }
-        return out;
+  /**
+   * Returns a string representation of the IfExpression.
+   * @returns A string representation of the expression.
+   */
+  toString(): string {
+    let out = `if ${this.condition.toString()} ${this.consequence.toString()}`;
+    if (this.alternative !== null) {
+      out += `else ${this.alternative.toString()}`;
     }
+    return out;
+  }
 }
 
 /**
@@ -186,79 +182,95 @@ export class IfExpression implements Expression {
  * This is used for accessing elements in arrays or other indexable structures.
  */
 export class IndexExpression implements Expression {
-    token: Token;
-    left: Expression;
-    index: Expression;
+  token: Token;
+  left: Expression;
+  index: Expression;
 
-    /**
-     * Creates a new IndexExpression instance.
-     * @param token The token associated with this expression.
-     * @param left The expression being indexed (e.g., an array).
-     * @param index The expression representing the index.
-     */
-    constructor(token: Token, left: Expression, index: Expression) {
-        this.token = token;
-        this.left = left;
-        this.index = index;
-    }
+  /**
+   * Creates a new IndexExpression instance.
+   * @param token The token associated with this expression.
+   * @param left The expression being indexed (e.g., an array).
+   * @param index The expression representing the index.
+   */
+  constructor(token: Token, left: Expression, index: Expression) {
+    this.token = token;
+    this.left = left;
+    this.index = index;
+  }
 
-    expressionNode() {
-    }
+  expressionNode() {}
 
-    /**
-     * Returns the literal value of the token.
-     * @returns The literal value of the token.
-     */
-    tokenLiteral(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
 
-    /**
-     * Returns a string representation of the IndexExpression.
-     * @returns A string representation of the expression.
-     */
-    toString(): string {
-        return `(${this.left.toString()}[${this.index.toString()}])`;
-    }
+  /**
+   * Returns a string representation of the IndexExpression.
+   * @returns A string representation of the expression.
+   */
+  toString(): string {
+    return `(${this.left.toString()}[${this.index.toString()}])`;
+  }
 }
 
 /**
  * Represents a function call expression in the AST.
  */
 export class CallExpression implements Expression {
-    token: Token;
-    func: Expression;
-    args: Expression[];
+  token: Token;
+  func: Expression;
+  args: Expression[];
 
-    /**
-     * Creates a new CallExpression instance.
-     * @param token The token associated with this expression.
-     * @param func The expression representing the function being called.
-     * @param args An array of expressions representing the arguments to the function.
-     */
-    constructor(token: Token, func: Expression, args: Expression[]) {
-        this.token = token;
-        this.func = func;
-        this.args = args;
-    }
+  /**
+   * Creates a new CallExpression instance.
+   * @param token The token associated with this expression.
+   * @param func The expression representing the function being called.
+   * @param args An array of expressions representing the arguments to the function.
+   */
+  constructor(token: Token, func: Expression, args: Expression[]) {
+    this.token = token;
+    this.func = func;
+    this.args = args;
+  }
 
-    expressionNode() {
-    }
+  expressionNode() {}
 
-    /**
-     * Returns the literal value of the token.
-     * @returns The literal value of the token.
-     */
-    tokenLiteral(): string {
-        return this.token.literal;
-    }
+  /**
+   * Returns the literal value of the token.
+   * @returns The literal value of the token.
+   */
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
 
-    /**
-     * Returns a string representation of the CallExpression.
-     * @returns A string representation of the expression.
-     */
-    toString(): string {
-        const args = this.args.map((a) => a.toString()).join(", ");
-        return `${this.func.toString()}(${args})`;
-    }
+  /**
+   * Returns a string representation of the CallExpression.
+   * @returns A string representation of the expression.
+   */
+  toString(): string {
+    const args = this.args.map((a) => a.toString()).join(", ");
+    return `${this.func.toString()}(${args})`;
+  }
+}
+
+export class AssignmentExpression implements Expression {
+  constructor(
+    public token: Token,
+    public name: Identifier,
+    public value: Expression
+  ) {}
+
+  expressionNode() {}
+
+  tokenLiteral(): string {
+    return this.token.literal;
+  }
+
+  toString(): string {
+    return `${this.name.toString()} = ${this.value.toString()}`;
+  }
 }
