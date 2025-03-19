f4b37c2acb4ed7946c7bddc952acf8981e4f657d
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-20 02:13:48 +0530

    Initialize dark mode in App component and update MutantEditor import path
    - Added useEffect to set the document's root element to dark mode on component mount.
    - Updated the import path for MutantEditor to reflect its new location in the components/panel directory.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,13 @@
-import MutantEditor from "./components/Editor";
+import { useEffect } from "react";
+import MutantEditor from "./components/panel/Editor";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add("dark");
+  }, []);
+
   return (
     <div className="w-screen h-screen flex flex-col font-family-mono bg-tokyo-bg">
       <MutantEditor />
