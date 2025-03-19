a81bf1ba3088f0abbd55e7f845e15aafa06cf32c
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-20 01:19:52 +0530

    Add AST module with type definitions for literals, statements, and expressions
    - Introduced a new index.ts file in the AST module to define and export types for AST nodes, including literals, statements, and expressions.
    - Organized imports from various submodules to streamline the structure and improve code clarity.
    - Enhanced type safety and maintainability by consolidating AST-related types in one location.

---
+++ app/src/lang/ast/index.ts
@@ -0,0 +1,101 @@
+import { Program } from "./ast";
+import {
+  StringLiteral,
+  ArrayLiteral,
+  HashLiteral,
+  IntegerLiteral,
+  FunctionLiteral,
+  FStringLiteral,
+} from "./literal";
+import {
+  LetStatement,
+  ReturnStatement,
+  ExpressionStatement,
+  BlockStatement,
+  WhileStatement,
+  BreakStatement,
+  ContinueStatement,
+  ForStatement,
+  ConstStatement,
+  ClassDeclaration,
+  MethodDefinition,
+  ConstructorDefinition,
+  PropertyDefinition,
+} from "./statement";
+import {
+  InfixExpression,
+  PrefixExpression,
+  BooleanExpression,
+  IfExpression,
+  IndexExpression,
+  CallExpression,
+  AssignmentExpression,
+} from "./expression";
+
+type ASTLiterals =
+  | StringLiteral
+  | ArrayLiteral
+  | HashLiteral
+  | IntegerLiteral
+  | FunctionLiteral
+  | FStringLiteral;
+
+type ASTStatements =
+  | LetStatement
+  | ReturnStatement
+  | ExpressionStatement
+  | BlockStatement
+  | WhileStatement
+  | BreakStatement
+  | ContinueStatement
+  | ForStatement
+  | ConstStatement
+  | ClassDeclaration
+  | MethodDefinition
+  | ConstructorDefinition
+  | PropertyDefinition;
+
+type ASTExpressions =
+  | InfixExpression
+  | PrefixExpression
+  | BooleanExpression
+  | IfExpression
+  | IndexExpression
+  | CallExpression
+  | AssignmentExpression;
+
+type ASTNodes = ASTLiterals | ASTStatements | ASTExpressions | Program;
+
+export {
+  Program,
+  StringLiteral,
+  ArrayLiteral,
+  HashLiteral,
+  IntegerLiteral,
+  FunctionLiteral,
+  FStringLiteral,
+  LetStatement,
+  ReturnStatement,
+  ExpressionStatement,
+  BlockStatement,
+  WhileStatement,
+  BreakStatement,
+  ContinueStatement,
+  ForStatement,
+  ConstStatement,
+  ClassDeclaration,
+  MethodDefinition,
+  ConstructorDefinition,
+  PropertyDefinition,
+  InfixExpression,
+  PrefixExpression,
+  BooleanExpression,
+  IfExpression,
+  IndexExpression,
+  CallExpression,
+  AssignmentExpression,
+  type ASTNodes,
+  type ASTLiterals,
+  type ASTStatements,
+  type ASTExpressions,
+};
