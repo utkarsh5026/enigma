82f9deff91fe2930c1409b77d969d962243e2983
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-16 00:25:31 +0530

    Add StatementParserRegistry for managing statement parsers
    - Introduced StatementParserRegistry class to centralize the registration and management of various statement parsers, enhancing modularity and organization within the parsing system.
    - Implemented methods for adding, removing, and finding parsers, as well as a parseStatement method to handle statement parsing based on context.
    - Registered default parsers for let, const, return, while, for, break, continue, and block statements, streamlining the parsing process for these constructs.

---
+++ app/src/lang/parser/registry/statements-parser-registry.ts
@@ -0,0 +1,67 @@
+import {
+  LetStatementParser,
+  ConstStatementParser,
+  ReturnStatementParser,
+  WhileStatementParser,
+  ForStatementParser,
+  BreakStatementParser,
+  ContinueStatementParser,
+  BlockStatementParser,
+  ExpressionStatementParser,
+} from "../statements";
+import {
+  ExpressionParser,
+  Parser,
+  ParsingContext,
+  StatementParse,
+} from "../core";
+import { Statement } from "@/lang/ast/ast";
+
+export class StatementParserRegistry implements StatementParse {
+  private parsers: Parser<Statement>[] = [];
+
+  constructor(private expressionParser: ExpressionParser) {
+    this.registerDefaultParsers();
+  }
+
+  private registerDefaultParsers(): void {
+    this.addParser(new LetStatementParser(this.expressionParser));
+    this.addParser(new ConstStatementParser(this.expressionParser));
+    this.addParser(new ReturnStatementParser(this.expressionParser));
+
+    // Control flow
+    this.addParser(new WhileStatementParser(this, this.expressionParser));
+    this.addParser(new ForStatementParser(this, this.expressionParser));
+    this.addParser(new BreakStatementParser());
+    this.addParser(new ContinueStatementParser());
+
+    this.addParser(new BlockStatementParser(this));
+  }
+
+  findParser(context: ParsingContext): Parser<Statement> | null {
+    return this.parsers.find((parser) => parser.canParse(context)) || null;
+  }
+
+  addParser(parser: Parser<Statement>): void {
+    this.parsers.push(parser);
+  }
+
+  removeParser(parserType: new () => Parser<Statement>): void {
+    this.parsers = this.parsers.filter(
+      (parser) => !(parser instanceof parserType)
+    );
+  }
+
+  parseStatement(context: ParsingContext): Statement {
+    const parser = this.findParser(context);
+
+    if (parser) {
+      return parser.parse(context);
+    }
+
+    const expressionStatementParser = new ExpressionStatementParser(
+      this.expressionParser
+    );
+    return expressionStatementParser.parse(context);
+  }
+}
