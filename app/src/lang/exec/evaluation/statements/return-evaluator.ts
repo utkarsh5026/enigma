4680960a50de46633112c140a04ea061da70bc71
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-16 05:27:06 +0530

    Add ReturnStatementEvaluator for handling return statements
    - Introduced ReturnStatementEvaluator class to evaluate return statements within the execution context.
    - Implemented logic to evaluate the return value and return a ReturnValueObject or an error if applicable.
    - This addition enhances the execution capabilities of the language by properly managing return statements in the evaluation process.

---
+++ app/src/lang/exec/evaluation/statements/return-evaluator.ts
@@ -0,0 +1,26 @@
+import { ReturnStatement } from "@/lang/ast";
+import {
+  Environment,
+  BaseObject,
+  ReturnValueObject,
+} from "@/lang/exec/objects";
+import { EvaluationContext, NodeEvaluator } from "@/lang/exec/core";
+import { ObjectValidator } from "../validate";
+
+export class ReturnStatementEvaluator
+  implements NodeEvaluator<ReturnStatement>
+{
+  public evaluate(
+    node: ReturnStatement,
+    env: Environment,
+    context: EvaluationContext
+  ): BaseObject {
+    const value = context.evaluate(node.returnValue, env);
+
+    if (ObjectValidator.isError(value)) {
+      return value;
+    }
+
+    return new ReturnValueObject(value);
+  }
+}
