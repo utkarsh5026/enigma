73baf8138062a918b014f1091b2dd8a83d87d4ca
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-16 10:28:55 +0530

    Enhance IfExpressionEvaluator with detailed step tracking
    - Added before and after step logging for evaluating if expression conditions, consequences, and alternatives, improving traceability of the evaluation process.
    - Introduced context messages for each evaluation step, including error handling, enhancing clarity and debugging capabilities.
    - This enhancement provides better insight into the evaluation flow and error handling within if expressions, facilitating improved debugging and understanding of the evaluation context.

--- app/src/lang/exec/evaluation/expressions/if-expression-evaluator.ts
+++ app/src/lang/exec/evaluation/expressions/if-expression-evaluator.ts
@@ -10,22 +10,55 @@ export class IfExpressionEvaluator implements NodeEvaluator<IfExpression> {
     env: Environment,
     context: EvaluationContext
   ) {
+    context.addBeforeStep(node, env, `Evaluating if expression`);
     const { conditions, consequences, alternative } = node;
 
     for (let i = 0; i < conditions.length; i++) {
+      context.addBeforeStep(
+        node,
+        env,
+        `Evaluating if expression condition ${i} ${conditions[i].toString()}`
+      );
+
       const condition = context.evaluate(conditions[i], env);
 
       if (ObjectValidator.isError(condition)) {
+        context.addAfterStep(
+          node,
+          env,
+          condition,
+          `Error evaluating if expression condition ${i}: ${condition.message}`
+        );
         return condition;
       }
 
       if (condition.isTruthy()) {
-        return context.evaluate(consequences[i], env);
+        context.addBeforeStep(
+          node,
+          env,
+          `Evaluating if expression consequence ${i} ${consequences[
+            i
+          ].toString()}`
+        );
+        const result = context.evaluate(consequences[i], env);
+        context.addAfterStep(
+          node,
+          env,
+          result,
+          `If expression consequence ${i} evaluated: ${result.inspect()}`
+        );
+        return result;
       }
     }
 
     if (alternative !== null) {
-      return context.evaluate(alternative, env);
+      context.addBeforeStep(
+        node,
+        env,
+        `Evaluating if expression alternative ${alternative.toString()}`
+      );
+      const result = context.evaluate(alternative, env);
+      return result;
     }
 
     return NullObject.INSTANCE;
