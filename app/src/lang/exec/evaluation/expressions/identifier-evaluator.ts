436dab1d1804a6eee66178c2c39e325a1bdabbc3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-16 10:32:33 +0530

    Enhance IdentifierEvaluator with detailed step tracking
    - Added before and after step logging for evaluating identifiers, including context messages for found identifiers and built-in identifiers.
    - Improved error handling by logging detailed messages for identifier evaluation, enhancing traceability and debugging capabilities.
    - This enhancement provides better insight into the evaluation flow and error handling within identifier evaluations, facilitating improved debugging and understanding of the evaluation context.

--- app/src/lang/exec/evaluation/expressions/identifier-evaluator.ts
+++ app/src/lang/exec/evaluation/expressions/identifier-evaluator.ts
@@ -1,17 +1,39 @@
-import { NodeEvaluator } from "@/lang/exec/core";
+import { EvaluationContext, NodeEvaluator } from "@/lang/exec/core";
 import { Identifier } from "@/lang/ast/ast";
 import { Environment, BaseObject, ErrorObject } from "@/lang/exec/objects";
 import { isBuiltin, getBuiltin } from "@/lang/exec/builtins";
 
 export class IndentifierEvaluator implements NodeEvaluator<Identifier> {
-  public evaluate(node: Identifier, env: Environment): BaseObject {
+  public evaluate(
+    node: Identifier,
+    env: Environment,
+    context: EvaluationContext
+  ): BaseObject {
+    context.addBeforeStep(node, env, `Evaluating identifier: ${node.value}`);
     const value = env.get(node.value);
     if (value !== null) {
+      context.addAfterStep(
+        node,
+        env,
+        value,
+        `Identifier found: ${value.inspect()}`
+      );
       return value;
     }
 
     if (isBuiltin(node.value)) {
-      return getBuiltin(node.value) as BaseObject;
+      context.addBeforeStep(
+        node,
+        env,
+        `Evaluating builtin identifier: ${node.value}`
+      );
+      const result = getBuiltin(node.value) as BaseObject;
+      context.addAfterStep(
+        node,
+        env,
+        result,
+        `Builtin identifier found: ${result.inspect()}`
+      );
     }
 
     return new ErrorObject(`identifier not found: ${node.value}`);
