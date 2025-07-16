626f54d50e8fa33ad0769998dc4a8f51f2993a8e
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-16 05:51:58 +0530

    Add PrefixExpressionEvaluator for evaluating prefix expressions
    - Introduced PrefixExpressionEvaluator class to handle the evaluation of prefix expressions within the execution context.
    - Implemented the evaluate method to process prefix expressions, supporting logical negation and negation operations.
    - This addition enhances the evaluation capabilities of the language by providing a structured approach to handle prefix expressions, improving modularity and organization within the evaluation process.

---
+++ app/src/lang/exec/evaluation/expressions/prefix-evaluator.ts
@@ -0,0 +1,80 @@
+import { NodeEvaluator } from "@/lang/exec/core";
+import { PrefixExpression } from "@/lang/ast";
+import {
+  Environment,
+  BaseObject,
+  ErrorObject,
+  BooleanObject,
+  IntegerObject,
+} from "@/lang/exec/objects";
+import { EvaluationContext } from "@/lang/exec/core";
+import { ObjectValidator } from "../validate";
+
+export class PrefixExpressionEvaluator
+  implements NodeEvaluator<PrefixExpression>
+{
+  /**
+   * Evaluates a prefix expression.
+   * @param node - The prefix expression to evaluate.
+   * @param env - The environment to evaluate the expression in.
+   * @param context - The context to evaluate the expression in.
+   * @returns The result of the evaluation.
+   */
+  public evaluate(
+    node: PrefixExpression,
+    env: Environment,
+    context: EvaluationContext
+  ): BaseObject {
+    const right = context.evaluate(node.right, env);
+    if (ObjectValidator.isError(right)) {
+      return right;
+    }
+
+    return this.evalPrefixExpression(node.operator, right);
+  }
+
+  /**
+   * Evaluates a prefix expression.
+   * @param operator - The operator to evaluate.
+   * @param right - The right operand.
+   * @returns The result of the evaluation.
+   */
+  private evalPrefixExpression(
+    operator: string,
+    right: BaseObject
+  ): BaseObject {
+    if (operator === "!") {
+      return this.evalLogicalNotOperator(right);
+    }
+
+    if (operator === "-") {
+      return this.evalNegationOperator(right);
+    }
+
+    return new ErrorObject(
+      `unknown operator: ${operator}${right.type()}, You can only use ! or - operator with BOOLEAN or INTEGER`
+    );
+  }
+
+  private evalLogicalNotOperator(value: BaseObject): BaseObject {
+    if (ObjectValidator.isBoolean(value)) {
+      return new BooleanObject(!value.value);
+    }
+
+    if (ObjectValidator.isNull(value)) {
+      return new BooleanObject(true);
+    }
+
+    return new BooleanObject(!(value as BaseObject).isTruthy());
+  }
+
+  private evalNegationOperator(value: BaseObject): BaseObject {
+    if (ObjectValidator.isInteger(value)) {
+      return new IntegerObject(-value.value);
+    }
+
+    const errorMessage = `unknown operator: -${value.type()}, You can only use - operator with INTEGER like -5, -10, -100, -1000, etc.`;
+
+    return new ErrorObject(errorMessage);
+  }
+}
