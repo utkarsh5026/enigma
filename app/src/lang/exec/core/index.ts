bcb304d8798a6763c7ec0961e5b2abbdcaf731e9
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-19 01:28:37 +0530

    Enhance core module exports by adding Environment, BaseObject, and ObjectType
    - Included Environment class, BaseObject interface, and ObjectType enum in core module exports for improved structure and type safety.
    - This change facilitates better organization and access to essential components for the Mutant programming language.

--- app/src/lang/exec/core/index.ts
+++ app/src/lang/exec/core/index.ts
@@ -1,10 +1,15 @@
 import { LoopContext } from "./loop-context";
 import { NodeEvaluator, EvaluationContext } from "./interfaces";
 import { ObjectValidator } from "./validate";
+import { Environment } from "./environment";
+import { BaseObject, ObjectType } from "../objects";
 
 export {
   LoopContext,
   type NodeEvaluator,
   type EvaluationContext,
   ObjectValidator,
+  Environment,
+  type BaseObject,
+  ObjectType,
 };
