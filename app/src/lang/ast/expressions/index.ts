f0e6ac1ccec1ebd15b293b200644e4793a3e9ccf
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-19 08:15:31 +0530

    Add index file for AST expression exports
    - Created a new index.ts file to consolidate and export SuperExpression, ThisExpression, PropertyExpression, and CallExpression for easier access in the AST module.
    - This change enhances the organization of expression classes within the AST structure.

---
+++ app/src/lang/ast/expressions/index.ts
@@ -0,0 +1,6 @@
+import { SuperExpression } from "./super-expression";
+import { ThisExpression } from "./this-expression";
+import { PropertyExpression } from "./property-expression";
+import { CallExpression } from "./call-expression";
+
+export { SuperExpression, ThisExpression, PropertyExpression, CallExpression };
