8f5c9233b3dba9fe81c2acd29d23d0ed8e84e07c
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-22 05:48:02 +0530

    Add index file for editor components and hooks
    - Created a new index file in the editor component directory to export EnigmaEditor, useDirectValueOverlays, and useEditorHighlighting.
    - Included type export for EnigmaEditorRef to enhance type safety and usability in other parts of the application.

---
+++ app/src/components/editor/index.ts
@@ -0,0 +1,7 @@
+import EnigmaEditor from "./components/enigma-editor";
+import { useDirectValueOverlays } from "./hooks/use-editor-value-overlays";
+import { useEditorHighlighting } from "./hooks/use-editor-highlighting";
+import type { EnigmaEditorRef } from "./components/enigma-editor";
+
+export { EnigmaEditor, useDirectValueOverlays, useEditorHighlighting };
+export type { EnigmaEditorRef };
